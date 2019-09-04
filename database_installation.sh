199123b75ba2549479dbbd5b862adb1093ff4aad
Marti Municoy <martimunicoy@gmail.com>
2019-09-04 19:07:26 +0200

    Automatic installation of the MySQL database

---
+++ database_installation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing MySQL user and database for Irrigation Program"
+mysql -u root -p << MYSQL_SCRIPT
+DROP DATABASE IF EXISTS irrigation_db;
+CREATE DATABASE irrigation_db;
+GRANT ALL ON irrigation_db.* TO 'irrigation_program'@'localhost';
+SET PASSWORD FOR 'irrigation_program'@'localhost' = PASSWORD('1234');
+FLUSH PRIVILEGES;
+USE irrigation_db;
+CREATE TABLE slots ( id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+description VARCHAR(100),
+active BOOLEAN
+);
+CREATE TABLE schedule (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+time TIME NOT NULL
+);
+CREATE TABLE status (id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+label VARCHAR(20) NOT NULL, status BOOLEAN
+);
+INSERT INTO status (label, status) VALUES ('running', FALSE)
+MYSQL_SCRIPT
+
