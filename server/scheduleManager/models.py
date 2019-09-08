06b8f7a20b37a27d3983f5d8343d1654f7d1f448
Marti Municoy <martimunicoy@gmail.com>
2019-09-09 00:06:09 +0200

    Style enhancements

--- server/scheduleManager/models.py
+++ server/scheduleManager/models.py
@@ -38,6 +38,22 @@ class WeekDay(models.Model):
         return self.name
 
 
+WD_DICT = {'Monday': 1,
+           'Tuesday': 2,
+           'Wednesday': 3,
+           'Thursday': 4,
+           'Friday': 5,
+           'Saturday': 6,
+           'Sunday': 7}
+
+WD_DICT_R = {1: 'Monday',
+             2: 'Tuesday',
+             3: 'Wednesday',
+             4: 'Thursday',
+             5: 'Friday',
+             6: 'Saturday',
+             7: 'Sunday'}
+
 class IrrigationHour(models.Model):
     hour = models.TimeField()
     week_days = models.ManyToManyField(WeekDay)
@@ -48,5 +64,52 @@ class IrrigationHour(models.Model):
         ordering = ['hour_id',]
         db_table = 'irrigation_hours'
 
+    def print_weekdays_interval(self):
+        indexes_list = []
+        for wd in self.week_days.all():
+            indexes_list.append(WD_DICT[wd.name])
+
+        indexes_list.sort()
+
+        old_i = -1
+        n_neighbors = [0,] * 7
+
+        for i in indexes_list:
+            if (i == old_i + 1):
+                n_neighbors[old_i - 1] += 1
+                n_neighbors[i - 1] += 1
+            old_i = i
+
+        out = ''
+
+        for j, i in enumerate(indexes_list):
+            if (n_neighbors[i - 1] == 2):
+                continue
+
+            if (n_neighbors[i - 1] == 1):
+                if (i > 1):
+                    if (i < 7):
+                        if (n_neighbors[i - 2] == 0):
+                            out += WD_DICT_R[i]
+                            if (n_neighbors[i] == 2):
+                                out += '-'
+                        else:
+                            out += WD_DICT_R[i]
+                    else:
+                        out += WD_DICT_R[i]
+                else:
+                    out += WD_DICT_R[i]
+                    if (n_neighbors[i] == 2):
+                        out += '-'
+
+            if (n_neighbors[i - 1] == 0):
+                out += WD_DICT_R[i]
+
+            if (j < len(indexes_list) - 1):
+                if (out[-1] != '-'):
+                    out += ', '
+
+        return out
+
     def __str__(self):
-        return str(self.hour)
+        return '{}: {}'.format(self.print_weekdays_interval(), self.hour)
